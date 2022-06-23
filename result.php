45dc89d877ce3e951d1b2176b493788b9c688161
NaoyaSato708 <NaoyaSato708@users.noreply.github.com>
2022-06-23 11:58:57 +0900

    ver.2.0

--- result.php
+++ result.php
@@ -16,64 +16,91 @@ $c = $_POST["c"];
 $pdo = new PDO(
     "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
 );
-//顧客毎の購入履歴を求める
+?>
+
+<?php//顧客毎の購入履歴を求める?>
+<h3>顧客別購入商品一覧</h3>
+<?php
+echo '顧客名：佐藤尚哉<br>';
 $e = $pdo -> query("SELECT * FROM client_info WHERE client_name = '佐藤尚哉'");
 while ($f = $e -> fetch()) {
-print "{$f['client_name']}: {$f['product_name']} {$f['price']}<br><hr>";}
+print "商品名：{$f['product_name']}  値段：{$f['price']}円<br><hr>";}
+$g = $pdo -> query("SELECT SUM(price) as sum_price FROM client_info WHERE client_name = '佐藤尚哉'");
+while ($h = $g -> fetch()) {
+print "合計金額：{$h['sum_price']}円<br><hr><br>";}
 
-
-
+echo '顧客名：藤原花子<br>';
 $e = $pdo -> query("SELECT * FROM client_info WHERE client_name = '藤原花子'");
 while ($f = $e -> fetch()) {
-print "{$f['client_name']}: {$f['product_name']} {$f['price']}<br><hr>";}
+print "商品名：{$f['product_name']}  値段：{$f['price']}円<br><hr>";}
+$g = $pdo -> query("SELECT SUM(price) as sum_price FROM client_info WHERE client_name = '藤原花子'");
+while ($h = $g -> fetch()) {
+print "合計金額：{$h['sum_price']}円<br><hr><br>";}
 
+
+echo '顧客名：山田太郎<br>';
 $e = $pdo -> query("SELECT * FROM client_info WHERE client_name = '山田太郎'");
 while ($f = $e -> fetch()) {
-print "{$f['client_name']}: {$f['product_name']} {$f['price']}<br><hr>";}
+print "商品名：{$f['product_name']}  値段：{$f['price']}円<br><hr>";}
+$g = $pdo -> query("SELECT SUM(price) as sum_price FROM client_info WHERE client_name = '山田太郎'");
+while ($h = $g -> fetch()) {
+print "合計金額：{$h['sum_price']}円<br><hr><br>";}
 
+echo '顧客名：加藤浩次<br>';
 $e = $pdo -> query("SELECT * FROM client_info WHERE client_name = '加藤浩次'");
 while ($f = $e -> fetch()) {
-print "{$f['client_name']}: {$f['product_name']} {$f['price']}<br><hr>";}
+print "商品名：{$f['product_name']}  値段：{$f['price']}円<br><hr>";}
+$g = $pdo -> query("SELECT SUM(price) as sum_price FROM client_info WHERE client_name = '加藤浩次'");
+while ($h = $g -> fetch()) {
+print "合計金額：{$h['sum_price']}円<br><hr><br>";}
+?>
 
+<h3>購入履歴</h3>
+<?php
 if(!empty($_POST["a"]) && !empty($_POST["b"]) && !empty($_POST["c"])) {
 $pdo = new PDO(
     "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
 );
+/*
 if ($pdo) {
     echo "DB接続OK<br>";
 } else {
     echo "DB接続NG<br>";
 }
-
+*/
 $pdo -> query("INSERT INTO
 client_info
 (client_name,product_name,price)
 VALUES
 ('$a','$b','$c')");
-
+/*
 if ($pdo) {
     echo "登録成功<br>";
 } else {
     echo "登録失敗<br>";
 }
+*/
+?>
 
+<?php
 $n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
 while ($i = $n -> fetch()) {
-print "{$i['client_name']}: {$i['product_name']}: {$i['price']}<br><hr>";}
+print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
 }
 else 
 {
     $pdo = new PDO(
         "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
     );
+    /*
     if ($pdo) {
         echo "DB接続OK<br>";
     } else {
         echo "DB接続NG<br>";
     }
+    */
     $n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
     while ($i = $n -> fetch()) {
-    print "{$i['client_name']}: {$i['product_name']} {$i['price']}<br><hr>";}
+    print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
 }
-
 ?>
